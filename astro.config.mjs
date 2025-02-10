f26f244aa4b61869dccd90241f99cc822ed46d06
Jantz Carney <jantzillac.93@gmail.com>
2025-02-09 20:54:04 -0600

    Add tailwind css to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
-
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-build-blog.netlify.app",
-  integrations: [preact(), svelte()]
+  integrations: [preact(), svelte()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
